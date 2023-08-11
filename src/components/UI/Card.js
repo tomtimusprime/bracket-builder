6bff8aaf2fb82c0c2a1328197ab1c442a2ff7bae
Tom Black <tom.black06007@gmail.com>
2023-08-11 14:48:07 -0700

    added a text input to the generated cards.

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,10 +1,15 @@
+import {useState} from 'react'
 import './Card.css';
 
 function Card(props) {
     const classes = 'card ' + props.className;
-
+    const [cardValue, setCardValue] = useState('');
     return (
-        <div className={classes}>{props.children}</div>
+        <div className={classes}>{props.children}
+        <p>{cardValue}</p>
+        <input type="text" value={cardValue} onChange={(e) =>  setCardValue(e.target.value)}></input>
+        </div>
+        
     )
 }
 
